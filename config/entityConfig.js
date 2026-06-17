7ebd5b3f23ee4564d1746fca9cb1bf2acefa9c0b
JJOJJD <david2626714@gmail.com>
2026-06-16 22:25:25 -0500

    refactor: Update Student schema to use English-only attributes and add semester

--- config/entityConfig.js
+++ config/entityConfig.js
@@ -7,8 +7,8 @@ const entityConfig = {
     email: { type: "String", required: false },
     course: { type: "String", required: false },
     age: { type: "Number", required: false },
+    semester: { type: "Number", required: false },
     grade: { type: "Number", required: true },
-    nota: { type: "Number", required: false },
     passed: { type: "Boolean", required: false },
     average: { type: "Number", required: false }
   },
